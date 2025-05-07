e8bef901357f40ae922d4bdd57716e8af2c44817
stevenpardoo <71938564+stevenpardoo@users.noreply.github.com>
2025-05-07 18:20:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import { chromium } from 'playwright';
 
+const { USER_ID, USER_PWD } = process.env;
+if (!USER_ID || !USER_PWD) {
+  throw new Error('ENV vars USER_ID/USER_PWD are missing');
+}
+
 (async () => {
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
   await page.goto('https://schoolpack.smart.edu.co/idiomas/alumnos.aspx');
-
-  await page.fill('input[name="vUSUCOD"]', process.env.USER_ID);
-  await page.fill('input[name="vPASS"]', process.env.USER_PWD);
+  await page.fill('input[name="vUSUCOD"]', USER_ID);
+  await page.fill('input[name="vPASS"]', USER_PWD);
   await page.click('input[type="submit"]');
 
-  // Aquí irán los pasos siguientes de navegación
+  // TODO: pasos de reserva
 
   await browser.close();
 })();
